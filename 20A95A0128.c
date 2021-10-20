93dbb8169a501a2cf858330f0bca530db926290c
innurahman <92291179+innurahman@users.noreply.github.com>
2021-10-20 12:34:42 +0530

    Add files via upload

---
+++ 20A95A0128.c
@@ -0,0 +1,24 @@
+/* c program to ptint data */
+#include<stdio.h>
+void main()
+{
+	// variable Declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initilization
+	age = 21;
+	cgpa = 8.7;
+	initial = 'k';
+	
+	//output
+	printf("age of the student is: %i\n",age);
+	printf("cgpa of the student is: %f\n",cgpa);
+	printf("initial of the student: %c\n",initial);
+	
+	
+}
+
+	
+	
