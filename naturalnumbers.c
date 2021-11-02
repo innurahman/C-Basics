b5490f88ceb889c116c4428f84824b0eadc7cd49
innurahman <92291179+innurahman@users.noreply.github.com>
2021-11-02 12:28:42 +0530

    Add files via upload

---
+++ naturalnumbers.c
@@ -0,0 +1,13 @@
+/* sum of natural numbers */
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int n,s;
+	//initialization
+	n=10;
+	//calculation
+	s=n*(n+1)/2;
+	//output
+	printf("sum of first %d natural numbers is %d:",n,s);
+}
