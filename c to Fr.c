f4d7240eb6e309921fd6ca4cc1d1d1485a325745
innurahman <92291179+innurahman@users.noreply.github.com>
2021-11-03 12:35:03 +0530

    Add files via upload

---
+++ c to Fr.c
@@ -0,0 +1,10 @@
+/*convert celsius (c) to fahenheit(F) */
+#include <stdio.h>
+void main()
+{
+float celsius,fahrenheit  ;
+celsius = 10;
+fahrenheit=(celsius*9/5)+32 ;
+
+printf("%f fahrenheit",fahrenheit );
+}
